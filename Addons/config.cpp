1af5e5c7888e84108fcf683454974f3d5cfe9fe6
prazv <64920464+prazv@users.noreply.github.com>
2025-05-12 15:12:58 +0300

    Add files via upload

---
+++ Addons/config.cpp
@@ -0,0 +1,61 @@
+class CfgPatches
+{
+    class NVG_Rebalance
+    {
+        requiredaddons[]={};
+        requiredversion= 1.0;
+        units[]={};
+        weapons[]={};
+        name = "NVG Weight Rebalance";
+        author = "prazv";
+    };
+};
+class CfgWeapons
+{
+	class NVGoggles;
+	
+	class JAS_GPNVG18_Tan_WP: NVGoggles
+	{
+        mass = 24;
+    };
+    class JAS_GPNVG18_blk: NVGoggles
+    {
+        mass = 24;
+    };
+    class JAS_GPNVG18_blk_WP: NVGoggles
+    {
+        mass = 24;
+    };
+    class JAS_GPNVG18_Tan: NVGoggles
+    {
+        mass = 24;
+    };
+    class JAS_GPNVG18_blk_TI: NVGoggles
+    {
+        mass = 24;
+    };
+    class JAS_GPNVG18_Tan_TI: NVGoggles
+    {
+        mass = 24;
+    };
+    class UK3CB_ANPVS7: NVGoggles
+    {
+        mass = 16;
+    };
+    class UK3CB_PVS5A: NVGoggles
+    {
+        mass = 16;
+    };
+    class rhsusf_ANPVS_14:NVGoggles
+    {
+        mass = 14;
+    };
+    class rhsusf_ANPVS_15
+    {
+        mass = 20;
+    };
+    class rhsusf_Rhino
+    {
+        mass = 5;
+    };
+};
---
+++ meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 0;
+name = "NVG weight balance";
+timestamp = 5250512503676284570;
